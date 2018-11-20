07b224a892910d4d0acf579ee0986471eac33f6c
Andrea Carlo Marini <andrea.marini@cern.ch>
2018-11-20 10:53:05 +0100

    updating config

--- cmd.sh
+++ cmd.sh
@@ -1,11 +1,12 @@
 
+mkdir /eos/user/k/klute/Nero/2018_11_HGC
 for thr in 0; 
 do
     suffix="_nocut"
     CONDOR="--condor"
     #CONDOR=""
     PROXY="--proxy"
-    OUTDIR="/eos/user/k/klute/Nero/2018_11_HGC/Hbb_pu200_pt${pt}_thr${thr}${suffix}_condor/"
+    OUTDIR="/eos/user/k/klute/Nero/2018_11_HGC/Hbb_pu200_pt${pt}_thr${thr}${suffix}/"
     mkdir -p $OUTDIR
-    python sendOnBatch.py ${CONDOR} ${PROXY} -n 200 -q 1nd --options="maxEvents=100 thr=$thr pu=0 what=ggh_hbb" -e fake --put-in=${OUTDIR} -d mysub/Hbb_pu200_thr${thr}${suffix}_condor -i testHGCalL1T_cfg.py -o hgcalNtuples.root
+    python sendOnBatch.py ${CONDOR} ${PROXY} -n 200 -q 1nd --options="maxEvents=100 thr=$thr pu=0 what=ggh_hbb" -e fake --put-in=${OUTDIR} -d mysub/Hbb_pu200_thr${thr}${suffix} -i testHGCalL1T_cfg.py -o hgcalNtuples.root
 done
